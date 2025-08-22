5e9ee9fc2bba27b7706de8021a2c856a4e6a700b
Caio Queiroz <caiogqueiroz@gmail.com>
2025-08-22 13:30:21 -0300

    refactor: remove deprecated configuration files and streamline feature flag setup for v2

--- bruno-athaide-product-test/poc-config-v2/main.tf
+++ bruno-athaide-product-test/poc-config-v2/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-# Reference the shared module
+# Reference the shared module with product value
 module "shared" {
   source = "../shared"
 }
@@ -30,4 +30,34 @@ provider "configcat" {
   # Use environment variables directly
   basic_auth_username = var.configcat_basic_auth_username != "" ? var.configcat_basic_auth_username : null
   basic_auth_password = var.configcat_basic_auth_password != "" ? var.configcat_basic_auth_password : null
+}
+
+# Get product ID from shared module
+data "configcat_products" "my_products" {
+  name_filter_regex = module.shared.configcat_product_name
+}
+
+# Get production environment
+data "configcat_environments" "prod_environment" {
+  product_id = data.configcat_products.my_products.products.0.product_id
+  name_filter_regex = "Production"
+}
+
+# Create V2 config
+resource "configcat_config" "my_config" {
+  product_id         = data.configcat_products.my_products.products.0.product_id
+  name               = "My config v2"
+  description        = "My config description"
+  order              = 0
+  evaluation_version = "v2"
+}
+
+# Create segment
+resource "configcat_segment" "my_segment" {
+  product_id           = data.configcat_products.my_products.products.0.product_id
+  name                 = "Beta users"
+  description          = "Beta users' description"
+  comparison_attribute = "email"
+  comparator           = "sensitiveIsOneOf"
+  comparison_value     = "betauser1@example.com,betauser2@example.com"
 }
--- bruno-athaide-product-test/poc-config-v2/prod.tf
+++ bruno-athaide-product-test/poc-config-v2/prod.tf
@@ -1,114 +1,15 @@
-variable "config_id" {
-  type = string
-}
-
-variable "prod_environment_id" {
-  type = string
-}
-
-# resource "configcat_setting" "bool" {
-#   config_id = var.config_id
-#   key       = "BoolSettingKey"
-#   name      = "Bool feature flag"
-#   order     = 1
-# }
-
+# Create string setting
 resource "configcat_setting" "my_string_setting_v2" {
-  config_id    = var.config_id
+  config_id    = configcat_config.my_config.id
   key          = "StringSettingKey"
   name         = "String setting"
   order        = 2
   setting_type = "string"
 }
 
-# resource "configcat_setting_value_v2" "bool_setting_value" {
-#   environment_id = var.prod_environment_id
-#   setting_id     = configcat_setting.bool.id
-
-#   mandatory_notes = "mandatory notes"
-
-#   value = { bool_value = true }
-
-#   targeting_rules = [
-#     {
-#       conditions = [
-#         {
-#           user_condition = {
-#             comparison_attribute = "email"
-#             comparator           = "sensitiveTextEquals"
-#             comparison_value     = { string_value = "@configcat.com" }
-#           }
-#         },
-#         {
-#           user_condition = {
-#             comparison_attribute = "color"
-#             comparator           = "isOneOf"
-#             comparison_value = {
-#               list_values = [
-#                 { value = "#000000", hint = "black" },
-#                 { value = "red" },
-#               ]
-#             }
-#           }
-#         },
-#         {
-#           segment_condition = {
-#             segment_id = var.beta_segment_id
-#             comparator = "isIn"
-#           }
-#         },
-#         {
-#           prerequisite_flag_condition = {
-#             prerequisite_setting_id = configcat_setting.string.id
-#             comparator              = "doesNotEqual"
-#             comparison_value        = { string_value = "test" }
-#           }
-#         }
-#       ]
-#       value = { bool_value = true }
-#     },
-
-#     {
-#       conditions = [
-#         {
-#           user_condition = {
-#             comparison_attribute = "county"
-#             comparator           = "sensitiveTextEquals"
-#             comparison_value     = { string_value = "Hungary" }
-#           }
-#         },
-#       ],
-
-#       percentage_options = [
-#         {
-#           percentage = 50
-#           value      = { bool_value = true }
-#         },
-#         {
-#           percentage = 50
-#           value      = { bool_value = false }
-#         }
-#       ]
-#     },
-
-#     {
-#       percentage_options = [
-#         {
-#           percentage = 30
-#           value      = { bool_value = true }
-#         },
-#         {
-#           percentage = 70
-#           value      = { bool_value = false }
-#         }
-#       ]
-#     },
-#   ]
-# }
-
-
+# Create string setting value with V2 targeting rules
 resource "configcat_setting_value_v2" "string_setting_value" {
-  environment_id = var.prod_environment_id
+  environment_id = data.configcat_environments.prod_environment.environments.0.environment_id
   setting_id     = configcat_setting.my_string_setting_v2.id
 
   value = { string_value = "test" }
@@ -125,6 +26,101 @@ resource "configcat_setting_value_v2" "string_setting_value" {
         },
       ]
       value = { string_value = "custom" }
+    },
+  ]
+}
+
+# Create boolean setting
+resource "configcat_setting" "my_bool_setting_v2" {
+  config_id    = configcat_config.my_config.id
+  key          = "BoolSettingKey"
+  name         = "Bool feature flag"
+  order        = 1
+  setting_type = "boolean"
+}
+
+# Create boolean setting value with advanced targeting
+resource "configcat_setting_value_v2" "bool_setting_value" {
+  environment_id = data.configcat_environments.prod_environment.environments.0.environment_id
+  setting_id     = configcat_setting.my_bool_setting_v2.id
+
+  mandatory_notes = "mandatory notes"
+
+  value = { bool_value = true }
+
+  targeting_rules = [
+    {
+      conditions = [
+        {
+          user_condition = {
+            comparison_attribute = "email"
+            comparator           = "sensitiveTextEquals"
+            comparison_value     = { string_value = "@configcat.com" }
+          }
+        },
+        {
+          user_condition = {
+            comparison_attribute = "color"
+            comparator           = "isOneOf"
+            comparison_value = {
+              list_values = [
+                { value = "#000000", hint = "black" },
+                { value = "red" },
+              ]
+            }
+          }
+        },
+        {
+          segment_condition = {
+            segment_id = configcat_segment.my_segment.id
+            comparator = "isIn"
+          }
+        },
+        {
+          prerequisite_flag_condition = {
+            prerequisite_setting_id = configcat_setting.my_string_setting_v2.id
+            comparator              = "doesNotEqual"
+            comparison_value        = { string_value = "test" }
+          }
+        }
+      ]
+      value = { bool_value = true }
+    },
+
+    {
+      conditions = [
+        {
+          user_condition = {
+            comparison_attribute = "county"
+            comparator           = "sensitiveTextEquals"
+            comparison_value     = { string_value = "Hungary" }
+          }
+        },
+      ],
+
+      percentage_options = [
+        {
+          percentage = 50
+          value      = { bool_value = true }
+        },
+        {
+          percentage = 50
+          value      = { bool_value = false }
+        }
+      ]
+    },
+
+    {
+      percentage_options = [
+        {
+          percentage = 30
+          value      = { bool_value = true }
+        },
+        {
+          percentage = 70
+          value      = { bool_value = false }
+        }
+      ]
     },
   ]
 }
