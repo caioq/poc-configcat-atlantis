27aa4564d7fea4b4ed744ab444b212ad7a66aa5f
Caio Queiroz <caiogqueiroz@gmail.com>
2025-08-25 11:54:52 -0300

    add ff

---
+++ bruno-athaide-product-test/poc-config-v3/prod/my-string-setting-v2.tf
@@ -0,0 +1,31 @@
+# Create string setting
+resource "configcat_setting" "my_string_setting_v2" {
+  config_id    = configcat_config.my_config.id
+  key          = "StringSettingKey"
+  name         = "String setting"
+  order        = 0
+  setting_type = "string"
+}
+
+# Create string setting value with V2 targeting rules
+resource "configcat_setting_value_v2" "string_setting_value" {
+  environment_id = data.configcat_environments.prod_environment.environments.0.environment_id
+  setting_id     = configcat_setting.my_string_setting_v2.id
+
+  value = { string_value = "test" }
+
+  targeting_rules = [
+    {
+      conditions = [
+        {
+          user_condition = {
+            comparison_attribute = "email"
+            comparator           = "sensitiveTextEquals"
+            comparison_value     = { string_value = "@configcat.com" }
+          }
+        },
+      ]
+      value = { string_value = "custom" }
+    },
+  ]
+}
