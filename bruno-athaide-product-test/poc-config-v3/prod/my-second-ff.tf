1699c2e55cafa2e7583ae89ee03bd058e0487775
Caio Queiroz <caiogqueiroz@gmail.com>
2025-08-25 13:01:23 -0300

    creating second ff

---
+++ bruno-athaide-product-test/poc-config-v3/prod/my-second-ff.tf
@@ -0,0 +1,17 @@
+# Create a simple boolean feature flag
+resource "configcat_setting" "my_second_ff" {
+  config_id    = module.parent_config.config_id
+  key          = "MySecondFF"
+  name         = "My Second Feature Flag"
+  order        = 1
+  setting_type = "boolean"
+}
+
+# Create the feature flag value
+resource "configcat_setting_value_v2" "my_second_ff_value" {
+  environment_id = data.configcat_environments.prod_environment.environments.0.environment_id
+  setting_id     = configcat_setting.my_second_ff.id
+
+  value = { bool_value = false }
+
+}
